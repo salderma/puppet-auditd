0f2e8f8688f990f854fdb6e9d438501273f0dd22
salderma <salderman1@udayton.edu>
2014-01-29 13:07:16 -0500

    trying to fixup failed spec tests

--- spec/classes/auditd_spec.rb
+++ spec/classes/auditd_spec.rb
@@ -18,60 +18,54 @@ describe 'auditd' do
     it { should contain_package('audit').with_ensure('1.0.42') }
   end
 
-  describe 'Test standard installation with monitoring and firewalling' do
-    let(:params) { {:monitor => true , :firewall => true, :port => '42', :protocol => 'tcp' } }
+  describe 'Test standard installation with monitoring' do
+    let(:params) { {:monitor => true } }
     it { should contain_package('audit').with_ensure('present') }
     it { should contain_service('auditd').with_ensure('running') }
     it { should contain_service('auditd').with_enable('true') }
     it { should contain_file('auditd.conf').with_ensure('present') }
     it { should contain_monitor__process('auditd_process').with_enable('true') }
-    it { should contain_firewall('auditd_tcp_42').with_enable('true') }
   end
 
   describe 'Test decommissioning - absent' do
-    let(:params) { {:absent => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
+    let(:params) { {:absent => true, :monitor => true } }
     it 'should remove Package[audit]' do should contain_package('audit').with_ensure('absent') end
     it 'should stop Service[auditd]' do should contain_service('auditd').with_ensure('stopped') end
     it 'should not enable at boot Service[auditd]' do should contain_service('auditd').with_enable('false') end
     it 'should remove auditd configuration file' do should contain_file('auditd.conf').with_ensure('absent') end
     it { should contain_monitor__process('auditd_process').with_enable('false') }
-    it { should contain_firewall('auditd_tcp_42').with_enable('false') }
   end
 
   describe 'Test decommissioning - disable' do
-    let(:params) { {:disable => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
+    let(:params) { {:disable => true, :monitor => true } }
     it { should contain_package('audit').with_ensure('present') }
     it 'should stop Service[auditd]' do should contain_service('auditd').with_ensure('stopped') end
     it 'should not enable at boot Service[auditd]' do should contain_service('auditd').with_enable('false') end
     it { should contain_file('auditd.conf').with_ensure('present') }
     it { should contain_monitor__process('auditd_process').with_enable('false') }
-    it { should contain_firewall('auditd_tcp_42').with_enable('false') }
   end
 
   describe 'Test decommissioning - disableboot' do
-    let(:params) { {:disableboot => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
+    let(:params) { {:disableboot => true, :monitor => true } }
     it { should contain_package('audit').with_ensure('present') }
     it { should_not contain_service('auditd').with_ensure('present') }
     it { should_not contain_service('auditd').with_ensure('absent') }
     it 'should not enable at boot Service[auditd]' do should contain_service('auditd').with_enable('false') end
     it { should contain_file('auditd.conf').with_ensure('present') }
     it { should contain_monitor__process('auditd_process').with_enable('false') }
-    it { should contain_firewall('auditd_tcp_42').with_enable('true') }
   end
 
   describe 'Test noops mode' do
-    let(:params) { {:noops => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
+    let(:params) { {:noops => true, :monitor => true } }
     it { should contain_package('audit').with_noop('true') }
     it { should contain_service('auditd').with_noop('true') }
     it { should contain_file('auditd.conf').with_noop('true') }
     it { should contain_monitor__process('auditd_process').with_noop('true') }
-    it { should contain_monitor__process('auditd_process').with_noop('true') }
-    it { should contain_monitor__port('auditd_tcp_42').with_noop('true') }
-    it { should contain_firewall('auditd_tcp_42').with_noop('true') }
   end
 
   describe 'Test customizations - template' do
     let(:params) { {:template => "auditd/spec.erb" , :options => { 'opt_a' => 'value_a' } } }
+
     it 'should generate a valid template' do
       content = catalogue.resource('file', 'auditd.conf').send(:parameters)[:content]
       content.should match "fqdn: rspec.example42.com"
@@ -80,6 +74,7 @@ describe 'auditd' do
       content = catalogue.resource('file', 'auditd.conf').send(:parameters)[:content]
       content.should match "value_a"
     end
+
   end
 
   describe 'Test customizations - source' do
@@ -117,15 +112,9 @@ describe 'auditd' do
     it { should contain_monitor__process('auditd_process').with_tool('puppi') }
   end
 
-  describe 'Test Firewall Tools Integration' do
-    let(:params) { {:firewall => true, :firewall_tool => "iptables" , :protocol => "tcp" , :port => "42" } }
-    it { should contain_firewall('auditd_tcp_42').with_tool('iptables') }
-  end
-
   describe 'Test OldGen Module Set Integration' do
-    let(:params) { {:monitor => "yes" , :monitor_tool => "puppi" , :firewall => "yes" , :firewall_tool => "iptables" , :puppi => "yes" , :port => "42" , :protocol => 'tcp' } }
+    let(:params) { {:monitor => "yes" , :monitor_tool => "puppi" } }
     it { should contain_monitor__process('auditd_process').with_tool('puppi') }
-    it { should contain_firewall('auditd_tcp_42').with_tool('iptables') }
     it { should contain_puppi__ze('auditd').with_ensure('present') }
   end
 
